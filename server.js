bcd0d891814c78b58892b526a76c12e2c9761e10
Vincent <vinkang97@gmail.com>
2016-07-21 22:01:01 +0000

    Finish

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 var express = require('express')
+var requestIp = require('request-ip');
+var app = express()
+var useragent = require('useragent')
+
+
+var port = process.env.PORT || 8080
+app.listen(port, function () {
+  console.log('App listening on port 8080!');
+});
+
+app.get('/',function(req,res){
+    var info = {
+		ipaddress: requestIp.getClientIp(req),
+		language: req.headers["accept-language"].split(",")[0],
+		software: useragent.parse(req.headers['user-agent']).os.family
+	};
+	res.json(info);
+} )
